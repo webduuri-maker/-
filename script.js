928c83ca60be5bc28041649012075f746e6a7860
webduuri-maker <webduuri@gmail.com>
2025-12-26 23:27:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const hamburger = document.getElementById("hamburger");
 const sideMenu = document.getElementById("sideMenu");
+const closeBtn = document.getElementById("closeBtn");
 
+// нээх
 hamburger.addEventListener("click", () => {
-  sideMenu.classList.toggle("show");
+  sideMenu.classList.add("show");
+});
+
+// хаах (X)
+closeBtn.addEventListener("click", () => {
+  sideMenu.classList.remove("show");
+});
+
+// гадна дарвал хаагдана
+document.addEventListener("click", (e) => {
+  if (
+    sideMenu.classList.contains("show") &&
+    !sideMenu.contains(e.target) &&
+    !hamburger.contains(e.target)
+  ) {
+    sideMenu.classList.remove("show");
+  }
 });
