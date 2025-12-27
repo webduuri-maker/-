16e2f2825c3c3a5785122364969191f31a692238
webduuri-maker <webduuri@gmail.com>
2025-12-27 16:26:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,19 @@ const hamburger = document.getElementById("hamburger");
 const sideMenu = document.getElementById("sideMenu");
 const closeBtn = document.getElementById("closeBtn");
 
-// Нээх
 hamburger.addEventListener("click", (e) => {
   e.stopPropagation();
   sideMenu.classList.add("show");
 });
 
-// Хаах (X)
 closeBtn.addEventListener("click", () => {
   sideMenu.classList.remove("show");
 });
 
-// Menu дотор дарвал хаахгүй (линк ажиллана)
 sideMenu.addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
-// Гадна дарвал хаагдана
 document.addEventListener("click", () => {
   sideMenu.classList.remove("show");
 });
